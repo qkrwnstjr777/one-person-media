1885121328d7d6960865f98c85e2ed89224a23aa
hanmj97 <38197067+hanmj97@users.noreply.github.com>
2019-05-16 21:43:54 +0900

    Create geasipan.java

---
+++ test1/java Resources/src/geasipan/geasipan.java
@@ -0,0 +1,53 @@
+package geasipan;
+
+public class geasipan {
+	private String number;
+	private String title;
+	private String userID;
+	private String userTel;
+	private String content;
+	private String good;
+	private String notgood;
+	public String getnumber() {
+		return number;
+	}
+	public void setnumber(String number) {
+		this.number = number;
+	}
+	public String gettitle() {
+		return title;
+	}
+	public void settitle(String title) {
+		this.title = title;
+	}
+	public String getuserID() {
+		return userID;
+	}
+	public void setuserID(String userID) {
+		this.userID = userID;
+	}
+	public String getuserTel() {
+		return userTel;
+	}
+	public void setuserTel(String userTel) {
+		this.userTel = userTel;
+	}
+	public String getcontent() {
+		return content;
+	}
+	public void setcontent(String content) {
+		this.content = content;
+	}
+	public String getgood() {
+		return good;
+	}
+	public void setgood(String good) {
+		this.good = good;
+	}
+	public String getnotgood() {
+		return notgood;
+	}
+	public void setnotgood(String notgood) {
+		this.notgood = notgood;
+	}
+}
