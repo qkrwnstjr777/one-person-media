9198de0a5689a04950b8a277285ddae25c439738
hanmj97 <38197067+hanmj97@users.noreply.github.com>
2019-05-16 21:47:15 +0900

    Create User.java

---
+++ test1/Java Resources/src/user/User.java
@@ -0,0 +1,40 @@
+ package user;
+
+public class User {
+	private String userID;
+	private String userPassword;
+	private String userTel;
+	private String userGender;
+	private String userEmail;
+	
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public String getUserTel() {
+		return userTel;
+	}
+	public void setUserTel(String userTel) {
+		this.userTel = userTel;
+	}
+	public String getUserGender() {
+		return userGender;
+	}
+	public void setUserGender(String userGender) {
+		this.userGender = userGender;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+}
